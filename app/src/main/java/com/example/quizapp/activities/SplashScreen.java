c0b1976869fe9c14e1c952a0cb2d0847d376ff13
1md1405 <duyle140503@gmail.com>
2024-05-28 04:36:53 +0700

    [Duy] update result, desc

--- app/src/main/java/com/example/quizapp/activities/SplashScreen.java
+++ app/src/main/java/com/example/quizapp/activities/SplashScreen.java
@@ -1,4 +1,4 @@
-package com.example.quizapp;
+package com.example.quizapp.activities;
 
 import android.os.Bundle;
 
@@ -7,12 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-public class me extends AppCompatActivity {
+
+import com.example.quizapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_me);
+        setContentView(R.layout.activity_splash_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
