dc20d5984ba07b1b06e91a8674334299ce775582
4-21-EA-36-F\tqbao <tqbao4756@gmail.com>
2024-06-03 21:03:34 +0700

    no message

--- app/src/main/java/com/example/quizapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/quizapp/activities/LoginActivity.java
@@ -2,9 +2,12 @@ package com.example.quizapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +15,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import com.example.quizapp.R;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView btnRegiter;
     private TextView btnForgotPassword;
+    private TextInputEditText txtEmail, txtPassword;
+    private Button btnOnLogin;
+    private DatabaseAccess DB;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +38,34 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        btnOnLogin = (Button) findViewById(R.id.btnOnLogin);
         btnRegiter = (TextView) findViewById(R.id.btnRegister);
+        txtEmail = (TextInputEditText) findViewById(R.id.txtEmail);
+        txtPassword = (TextInputEditText) findViewById(R.id.txtPassword);
+        btnOnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(),
+                                    "Hãy nhập Email của bạn!!",
+                                    Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(),
+                                    "Hãy nhập mật khẩu của bạn!!",
+                                    Toast.LENGTH_LONG)
+                            .show();
+                    return;
+                }
+
+            }
+        });
         btnRegiter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +82,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
