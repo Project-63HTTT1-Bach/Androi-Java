c2a67df2d152de7315085edcae16fa81dbbd185b
Bach293 <bach01299929785@gmail.com>
2024-06-17 12:20:16 +0700

    [Bach] Update project structure

---
+++ app/src/main/java/com/example/quizapp/Auth/repositories/UserRepository.java
@@ -0,0 +1,87 @@
+package com.example.quizapp.Auth.repositories;
+
+import android.content.Context;
+
+import com.example.quizapp.models.User;
+import com.example.quizapp.sqliteOpenHelper.SqliteOpenHelper;
+
+import java.util.ArrayList;
+
+public class UserRepository {
+    private static ArrayList<User> userList = new ArrayList<>();
+    private final SqliteOpenHelper dbHelper;
+
+    public UserRepository(Context context) {
+        dbHelper = new SqliteOpenHelper(context);
+        if (userList.isEmpty()) {
+            userList = dbHelper.getAllUsers(); // Load from DB if the list is empty
+        }
+    }
+
+    public static ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public boolean checkExistedUser(User u) {
+        for (User user : userList) {
+            if (user.getFullname().equals(u.getFullname())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkUser(User u) {
+        for (User user : userList) {
+            if (user.getEmail().equals(u.getEmail())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addUser(User u) {
+        if (!checkExistedUser(u)) {
+            dbHelper.insertUser(u.getUsername(), u.getPassword(), u.getFullname(), u.getUserCode(), u.getEmail(), u.getProfilePicture());
+            userList.add(u); // Add to the static list
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //    public void removeUser(User u) {
+//        dbHelper.deleteUser(u.getUsername());
+//        userList.remove(u); // Remove from the static list
+//    }
+//
+    public User getUser(String email) {
+        for (User user : userList) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<User> getAllUsers() {
+        return userList;
+    }
+
+    public int getUserCount() {
+        return userList.size();
+    }
+
+    public boolean updateUser(User u) {
+        for (User user : userList) {
+            if (user.getEmail().equals(u.getEmail())) {
+                user.setProfilePicture(u.getProfilePicture());
+                user.setFullname(u.getFullname());
+                user.setPassword(u.getPassword());
+                dbHelper.updateUser(u.getUserId(), u.getUsername(), u.getPassword(), u.getFullname(), u.getUserCode(), u.getEmail(), u.getProfilePicture());
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/Quiz/models/Result.java
@@ -0,0 +1,77 @@
+package com.example.quizapp.Quiz.models;
+
+public class Result {
+    private int resultId;
+    private int userId;
+    private int quizId;
+    private int score;
+    private String completionDate;
+    private int correctAnswers;
+    private int incorrectAnswers;
+
+    public Result(int resultId, int userId, int quizId, int score, String completionDate, int correctAnswers, int incorrectAnswers) {
+        this.resultId = resultId;
+        this.userId = userId;
+        this.quizId = quizId;
+        this.score = score;
+        this.completionDate = completionDate;
+        this.correctAnswers = correctAnswers;
+        this.incorrectAnswers = incorrectAnswers;
+    }
+
+    public int getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(int resultId) {
+        this.resultId = resultId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(int quizId) {
+        this.quizId = quizId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(String completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public int getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(int correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
+
+    public int getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
+    public void setIncorrectAnswers(int incorrectAnswers) {
+        this.incorrectAnswers = incorrectAnswers;
+    }
+}
