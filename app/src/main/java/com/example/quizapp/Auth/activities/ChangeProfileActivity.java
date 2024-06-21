e0cecb65114fd5085666b3ac11ada19cb1de2d81
4-21-EA-36-F\tqbao <tqbao4756@gmail.com>
2024-06-21 16:45:21 +0700

    [Bao] update setting, change info profile

--- app/src/main/java/com/example/quizapp/Auth/activities/ChangeProfileActivity.java
+++ app/src/main/java/com/example/quizapp/Auth/activities/ChangeProfileActivity.java
@@ -1,19 +1,38 @@
 package com.example.quizapp.Auth.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quizapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChangeProfileActivity extends AppCompatActivity {
     private ImageView btnBack;
+    private TextView tvEmail;
+    private EditText etFullname, etBirthday, etPhone;
+    private LinearLayout btnSave;
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
+    private String userEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +49,96 @@ public class ChangeProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        btnSave = findViewById(R.id.btnSave);
+        etFullname = findViewById(R.id.etFullname2);
+        etBirthday = findViewById(R.id.etBirthday2);
+        etPhone = findViewById(R.id.etPhone2);
+        tvEmail = findViewById(R.id.tvEmail2);
+
+        database = FirebaseDatabase.getInstance();
+
+        // Nhận email từ Intent
+        userEmail = getIntent().getStringExtra("userEmail");
+
+        loadUserData(userEmail);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserData();
+            }
+        });
+    }
+
+    private void loadUserData(String email) {
+        userRef = database.getReference("users");
+        userRef.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                        String fullName = userSnapshot.child("fullname").getValue(String.class);
+                        String birthday = userSnapshot.child("birthday").getValue(String.class);
+                        String phone = userSnapshot.child("phone").getValue(String.class);
+                        String email = userSnapshot.child("email").getValue(String.class);
+
+                        etFullname.setText(fullName);
+                        etBirthday.setText(birthday);
+                        etPhone.setText(phone);
+                        tvEmail.setText(email);
+                    }
+                } else {
+                    // Handle trường hợp không tìm thấy người dùng
+                    Log.e("ChangeProfileActivity", "User not found.");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle lỗi
+                Log.e("ChangeProfileActivity", "Database error: ", error.toException());
+            }
+        });
+    }
+
+    private void updateUserData() {
+        String newFullName = etFullname.getText().toString().trim();
+        String newBirthday = etBirthday.getText().toString().trim();
+        String newPhone = etPhone.getText().toString().trim();
+        String email = tvEmail.getText().toString().trim();
+
+        userRef.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                        String userId = userSnapshot.getKey();
+                        if (userId != null) {
+                            userRef.child(userId).child("fullname").setValue(newFullName);
+                            userRef.child(userId).child("birthday").setValue(newBirthday);
+                            userRef.child(userId).child("phone").setValue(newPhone);
+                            Toast.makeText(ChangeProfileActivity.this, "Profile updated successfully", Toast.LENGTH_SHORT).show();
+
+                            // Quay lại SettingActivity với dữ liệu đã cập nhật
+                            Intent intent = new Intent(ChangeProfileActivity.this, SettingActivity.class);
+                            intent.putExtra("userEmail", userEmail);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                } else {
+                    // Handle trường hợp không tìm thấy người dùng
+                    Log.e("ChangeProfileActivity", "User not found.");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle lỗi
+                Log.e("ChangeProfileActivity", "Database error: ", error.toException());
             }
         });
     }
--- app/src/main/java/com/example/quizapp/Auth/activities/GithubLoginActivity.java
+++ app/src/main/java/com/example/quizapp/Auth/activities/GithubLoginActivity.java
@@ -1,19 +1,15 @@
 package com.example.quizapp.Auth.activities;
 
-import static java.security.AccessController.getContext;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quizapp.HomeAndDiscover.activities.MainActivity;
 import com.example.quizapp.R;
@@ -23,15 +19,20 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.OAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class GithubLoginActivity extends AppCompatActivity {
     TextInputEditText txtEmail;
@@ -40,17 +41,13 @@ public class GithubLoginActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseDatabase database;
     UserRepository userRepository;
+    DatabaseReference userRef;
+    DatabaseReference userIdRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_github_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         txtEmail = findViewById(R.id.txtEmail);
         btnLogin = findViewById(R.id.btnLogin);
@@ -61,82 +58,161 @@ public class GithubLoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = txtEmail.getText().toString();
-                if(!email.matches(emailPattern)){
-                    Toast.makeText(GithubLoginActivity.this, "Enter a proper email", Toast.LENGTH_SHORT).show();
-                } else {
-                    OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
-                    provider.addCustomParameter("login", email);
-                    List<String> scopes = new ArrayList<String>() {{
-                        add("user:email");
-                    }};
-                    provider.setScopes(scopes);
+                handleGithubLogin();
+            }
+        });
+    }
 
-                    Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
-                    if (pendingResultTask != null) {
-                        pendingResultTask
-                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                    @Override
-                                    public void onSuccess(AuthResult authResult) {
-                                        handleSignInResult(authResult);
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(GithubLoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+    private void handleGithubLogin() {
+        String email = txtEmail.getText().toString().trim();
+
+        if (email.matches(emailPattern)) {
+            signInWithGithub(email);
+        } else {
+            Toast.makeText(GithubLoginActivity.this, "Invalid Email Address", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void signInWithGithub(String email) {
+        OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+        provider.addCustomParameter("login", email);
+
+        Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
+        if (pendingResultTask != null) {
+            pendingResultTask.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    handleSignInResult(authResult);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(GithubLoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            mAuth.startActivityForSignInWithProvider(this, provider.build()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    handleSignInResult(authResult);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(GithubLoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void handleSignInResult(AuthResult authResult) {
+        FirebaseUser user = authResult.getUser();
+        if (user != null) {
+            linkProviderWithExistingUser(user, OAuthProvider.newCredentialBuilder("github.com").build());
+        }
+    }
+
+    private void linkProviderWithExistingUser(FirebaseUser user, AuthCredential credential) {
+        String userEmail = user.getEmail();
+        if (userEmail != null) {
+            userRef = database.getReference("users");
+            userRef.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        // Email exists in the database, link the provider
+                        user.linkWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                proceedToMainActivity();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(GithubLoginActivity.this, "Failed to link account", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     } else {
-                        mAuth
-                                .startActivityForSignInWithProvider(GithubLoginActivity.this, provider.build())
-                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                    @Override
-                                    public void onSuccess(AuthResult authResult) {
-                                        handleSignInResult(authResult);
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(GithubLoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+                        // Email does not exist in the database, create a new user
+                        createUserWithCurrentID(user);
                     }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("DatabaseError", "onCancelled: ", error.toException());
+                }
+            });
+        }
+    }
+
+    private void createUserWithCurrentID(FirebaseUser user) {
+        userIdRef = database.getReference("currentID");
+        userIdRef.runTransaction(new Transaction.Handler() {
+            @NonNull
+            @Override
+            public Transaction.Result doTransaction(@NonNull MutableData currentData) {
+                Integer currentId = currentData.getValue(Integer.class);
+                if (currentId == null) {
+                    currentData.setValue(1);
+                } else {
+                    currentData.setValue(currentId + 1);
+                }
+                return Transaction.success(currentData);
+            }
+
+            @Override
+            public void onComplete(@Nullable DatabaseError error, boolean committed, @Nullable DataSnapshot currentData) {
+                if (committed) {
+                    Integer newId = currentData.getValue(Integer.class);
+                    if (newId != null) {
+                        String email = user.getEmail();
+                        String fullName = user.getDisplayName();
+                        String avatarUrl = user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "";
+
+                        // Create new user object
+                        User newUser = new User(newId, "", "", fullName, email, avatarUrl, "01/01/2003", "");
+
+                        // Add user to SQLite
+                        userRepository.addUser(newUser);
+
+                        // Add user data to Firebase Realtime Database
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("id", newId);
+                        userMap.put("email", email);
+                        userMap.put("fullname", fullName);
+                        userMap.put("profilePicture", avatarUrl);
+                        userMap.put("birthday", "01/01/2003");
+                        userMap.put("username", "");
+                        userMap.put("password", "");
+                        userMap.put("phone", "");
+
+                        // Save user to Firebase
+                        database.getReference().child("users").child(newId.toString()).setValue(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(GithubLoginActivity.this, "User added successfully", Toast.LENGTH_SHORT).show();
+                                proceedToMainActivity();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(GithubLoginActivity.this, "Failed to add user", Toast.LENGTH_SHORT).show();
+                                Log.e("DatabaseError", "onFailure: ", e);
+                            }
+                        });
+                    }
+                } else {
+                    Log.e("GithubLoginActivity", "Transaction failed.");
                 }
             }
         });
     }
 
-    private void handleSignInResult(AuthResult authResult) {
-        FirebaseUser user = mAuth.getCurrentUser();
-
-        if (user != null) {
-            String userId = user.getUid();
-            String userName = user.getDisplayName();
-            String userEmail = user.getEmail();
-            String userAvatar = (user.getPhotoUrl() != null) ? user.getPhotoUrl().toString() : "";
-
-            // Thêm vào Firebase Realtime Database
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("id", userId);
-            map.put("name", userName);
-            map.put("email", userEmail);
-            map.put("avatar", userAvatar);
-            database.getReference().child("users").child(userId).setValue(map);
-
-            User newUser = new User(userName, userEmail, userAvatar);
-
-            // Thêm vào SQLite
-            if (!userRepository.checkUser(newUser)) {
-                userRepository.addUser(newUser);
-            }
-
-            // Chuyển hướng người dùng đến màn hình chính
-            Intent intent = new Intent(GithubLoginActivity.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            finish();
-        }
+    private void proceedToMainActivity() {
+        Intent intent = new Intent(GithubLoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
