6dabbd78ed393e588436b4a9ac52bb8bfddd84ff
4-21-EA-36-F\tqbao <tqbao4756@gmail.com>
2024-06-21 14:53:46 +0700

    [Bao] pull code

--- app/src/main/java/com/example/quizapp/Auth/activities/LoginActivity.java
+++ app/src/main/java/com/example/quizapp/Auth/activities/LoginActivity.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,11 +20,6 @@ import com.example.quizapp.R;
 import com.example.quizapp.Auth.models.User;
 import com.example.quizapp.Auth.repositories.UserRepository;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -34,10 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     private UserRepository userRepository;
     private Button btnOnLogin;
     private LinearLayout btnOnLoginGoogle, btnOnLoginGithub;
-    private TextView btnRegister;
+    private TextView btnRegiter;
     private TextView btnForgotPassword;
-    private FirebaseDatabase database;
-    private DatabaseReference userRef;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
@@ -53,34 +45,33 @@ public class LoginActivity extends AppCompatActivity {
         tietEmail = findViewById(R.id.txtEmail);
         tietPassword = findViewById(R.id.txtPassword);
         btnOnLogin = findViewById(R.id.btnOnLogin);
-        btnRegister = findViewById(R.id.btnRegister);
+        btnRegiter = findViewById(R.id.btnRegister);
         btnOnLoginGoogle = findViewById(R.id.btnOnLoginGoogle);
         btnOnLoginGithub = findViewById(R.id.btnOnLoginGithub);
 
         userRepository = new UserRepository(this);
-        database = FirebaseDatabase.getInstance();
-        userRef = database.getReference("users");
+//        initData();
 
         ArrayList<User> userList = userRepository.getAllUsers();
         for (User user : userList) {
-            Log.d("UserRepository", "User: " + user.getFullname() + ", Email: " + user.getEmail() + ", Password: " + user.getPassword());
+            Log.d("UserRepository", "User: " + user.getFullname() + ", Email: " + user.getEmail() + ", Password: " + user.getPassword() );
         }
-
         btnOnLogin.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 String email = tietEmail.getText().toString().trim();
                 String password = tietPassword.getText().toString().trim();
-                if (!email.matches(emailPattern)) {
+                if(!email.matches(emailPattern)){
                     Toast.makeText(LoginActivity.this, "Enter a proper email", Toast.LENGTH_SHORT).show();
-                } else {
+                }
+                else{
                     loginUser(email, password);
                 }
             }
         });
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnRegiter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -97,14 +88,15 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
         btnOnLoginGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, GoogleLoginActivity.class);
                 startActivity(intent);
+
             }
         });
-
         btnOnLoginGithub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -115,31 +107,30 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String email, String password) {
-        userRef.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    for (DataSnapshot userSnapshot : snapshot.getChildren()) {
-                        String dbPassword = userSnapshot.child("password").getValue(String.class);
-                        if (dbPassword != null && dbPassword.equals(password)) {
-                            Toast.makeText(LoginActivity.this, "Login success!!!!", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                            return;
-                        }
-                    }
-                    Toast.makeText(LoginActivity.this, "Invalid password!!!!", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Email does not exist!!!!", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e("LoginActivity", "Database error: ", error.toException());
-                Toast.makeText(LoginActivity.this, "Login failed!!!!", Toast.LENGTH_LONG).show();
-            }
-        });
+        User user = userRepository.getUser(email);
+        if (user != null && user.getPassword().equals(password)) {
+            Toast.makeText(LoginActivity.this, "OnLogin success!!!!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(LoginActivity.this, "OnLogin failed!!!!", Toast.LENGTH_LONG).show();
+        }
     }
+
+//    private void initData() {
+//        for (int i = 0; i < 10; i++) {
+//            int id = i+1;
+//            String name = "username" + (i + 1);
+//            String email = "email" + (i + 1);
+//            String password = "password" + (i + 1);
+//            String fullname = "fullname" + i;
+//            String userCode = "2003-01-0" + (i + 1);
+//            String profilePicture = "profilePicture" +(i+1);
+//
+//            User user = new User(id, name, email, password, fullname, userCode, profilePicture);
+//            userRepository.addUser(user);
+//        }
+//    }
+
 }
